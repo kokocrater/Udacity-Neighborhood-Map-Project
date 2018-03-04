17622c97b1907515c3d44cc37d207545c53f9b4b
Scott <scott.jochim@att.net>
2018-03-04 08:53:34 -0600

    README.md: corrected typo; added to Search description.  script.js: changed var to let; refactored addListener() for markers.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-"use strict"
-var map;
-var markers = ko.observableArray();
-var titles = ko.observableArray();
-var matches = ko.observableArray();
-var locations = [
+"use strict";
+let map;
+let markers = ko.observableArray();
+let titles = ko.observableArray();
+let matches = ko.observableArray();
+let locations = [
     {title: "Vernon\'s BBQ", location: {lat: 38.662186, lng: -90.3091335}, keywords: ["restaurants", "food", "dining", "barbecue", "BBQ"]},
     {title: "Blueberry Hill", location: {lat: 38.655825, lng: -90.3051857}, keywords: ["restaurants", "food", "dining", "burgers", "beer"]},
     {title: "Grill Stop", location: {lat: 38.6721711, lng: -90.338078}, keywords: ["restaurants", "food", "dining", "burgers", "steak"]},
@@ -26,31 +26,35 @@ function initMap() {
     catch(err) {
         window.alert("Google maps failed to load: " + err.message);
     }
-  var infoWindow = new google.maps.InfoWindow();
+  let infoWindow = new google.maps.InfoWindow();
 
-  for (var i = 0; i < locations.length; i++) {
-    var position = locations[i].location;
-    var title = locations[i].title;
-    var label = (i+1).toString();
-    var marker = new google.maps.Marker({
+  for (let i = 0; i < locations.length; i++) {
+    let position = locations[i].location;
+    let title = locations[i].title;
+    let label = (i+1).toString();
+    let marker = new google.maps.Marker({
       position: position,
       title: title,
       id: i,
-      label: label,
+      label: label
     });
-    marker.addListener('click', function() {
-        openInfoWindow(this, infoWindow);
-        this.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(this.setAnimation(null), 1000); //https://developers.google.com/maps/documentation/javascript/examples/marker-animations
-    });
+    addMarkerListener(marker);
     markers().push(marker);
     titles.push(marker.title);
   }
+  function addMarkerListener(marker) {
+    marker.addListener('click', function() {
+        console.log(marker.title);
+        openInfoWindow(marker, infoWindow);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(marker.setAnimation(null), 1000); //https://developers.google.com/maps/documentation/javascript/examples/marker-animations
+    });
+}
     showMarkers(markers);
 }
 
 function showMarkers(markers) {
-    for (var i = 0; i < markers().length; i++) {
+    for (let i = 0; i < markers().length; i++) {
         markers()[i].setMap(map);
     }
 }
@@ -66,9 +70,9 @@ function openInfoWindow(marker, infoWindow) {
     });
     }
     //Since the foursquare api url doesn't accept place names (see: https://stackoverflow.com/questions/11583447/searching-venues-by-name-only),
-    //this var takes the position of the marker, converts it to a string and removes the parentheses and space to match the format of the
+    //this let takes the position of the marker, converts it to a string and removes the parentheses and space to match the format of the
     //foursquare 'll'.
-    var ll = marker.position.toString().slice(1, -1).replace(', ', ',');
+    let ll = marker.position.toString().slice(1, -1).replace(', ', ',');
 $.ajax({
     type: "GET",
     dataType: "jsonp",
@@ -77,10 +81,10 @@ $.ajax({
     'client_secret=20XNYOIX4IORXRZ2B43UVSZWL4ITW35T2NHODZNRXPECOVGC&v=20180227',
     success: function(data){
         //parse the data response into the fields to be added to the infoWindow
-        var address0 = data.response.groups[0].items[0].venue.location.formattedAddress[0];
-        var address1 = data.response.groups[0].items[0].venue.location.formattedAddress[1];
-        var phone = data.response.groups[0].items[0].venue.contact.formattedPhone;
-        var hours = data.response.groups[0].items[0].venue.hours.status;
+        let address0 = data.response.groups[0].items[0].venue.location.formattedAddress[0];
+        let address1 = data.response.groups[0].items[0].venue.location.formattedAddress[1];
+        let phone = data.response.groups[0].items[0].venue.contact.formattedPhone;
+        let hours = data.response.groups[0].items[0].venue.hours.status;
         if (!address0) {
             address0 = "Address is not available";
         }
@@ -112,13 +116,13 @@ function AppViewModel() {
         //with matching titles.  For markers() the matching indexes are compared to it.  Markers
         //at non-matching indexes are hidden.
         titles.removeAll();
-        for (var i = 0; i < locations.length; i++) {
+        for (let i = 0; i < locations.length; i++) {
             //If the text in the search box matches any location titles...
             if (this.search().toLowerCase() == locations[i].title.toLowerCase()) {
                 //push the index of the matching location to matches().
                 matches.push(i);
             } else {
-                for (var j = 0; j < locations[i].keywords.length; j++) {
+                for (let j = 0; j < locations[i].keywords.length; j++) {
                     //or is the text in the seach box matches any keyword...
                     if (this.search().toLowerCase() == locations[i].keywords[j].toLowerCase()) {
                         //push the index of the matching location to matches().
@@ -128,21 +132,21 @@ function AppViewModel() {
             }
         }
         //make the title(s) the only title(s) in the titles() observableArray.
-        for (var k = 0; k < matches().length; k++) {
-            var titleIndex = matches()[k];
-            var matchedTitle = locations[titleIndex].title;
+        for (let k = 0; k < matches().length; k++) {
+            let titleIndex = matches()[k];
+            let matchedTitle = locations[titleIndex].title;
             titles.push(matchedTitle);
         }
         //hide un-matched markers
-        for (var l = 0; l < matches().length; l++) {
+        for (let l = 0; l < matches().length; l++) {
             //Use the value of the current matches() item as the index of
             //a marker to be removed from markers()
-            var markerIndex = matches()[l];
+            let markerIndex = matches()[l];
             //Replace the removed marker with 'null' so that indexes of
             //other markers remain the same.
             markers.splice(markerIndex, 1, null);//https://www.w3schools.com/jsref/jsref_splice.asp AND https://blog.mariusschulz.com/2016/07/16/removing-elements-from-javascript-arrays
         }
-        for (var m = 0; m < markers().length; m++) {
+        for (let m = 0; m < markers().length; m++) {
             //Hide the markers that are not 'null'.
             if (markers()[m]) {
                 markers()[m].setMap(null);
